02a30034af4417d4267bed12897fd156db3caf5a
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-04-28 20:42:56 -0300

    refactor: Code refactor
    - Adds 4-space identation to modified files;
    - Refactor routes.js file;

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,4 +1,3 @@
-
 // Paginas do aplicativo
 import AboutCovidPage from '../pages/about-covid.f7.html';
 import InfectedPage from '../pages/infected.f7.html';
@@ -23,106 +22,149 @@ import RequestAndLoad from '../pages/request-and-load.f7.html';
 import NotFoundPage from '../pages/404.f7.html';
 
 var routes = [
-  {
-    path: '/',
-    component: HomePage,
-  },
-
-  { path: '/infected/', component: InfectedPage },
-  {
-      path: '/infected/:age/',
-      component: InfectedQuizPage,
-      on: {
-          pageInit: function (event, page) {
-            console.log(page.route.params.age);
-          },
-      },
-  },
-  { path: '/about-covid/', component: AboutCovidPage },
-  { path: '/symptoms/', component: SymptomsPage },
-  { path: '/risk-group/', component: RiskGroupPage },
-  { path: '/prevention/', component: PreventionPage },
-  { path: '/where-help/', component: WhereHelpPage },
-  { path: '/stats/', component: StatsPage },
-  { path: '/news/', component: NewsPage },
-  { path: '/mask/', component: MaskPage },
-  { path: '/about/', component: AboutPage },
-
-  {
-    path: '/form/',
-    component: FormPage,
-  },
-  {
-    path: '/catalog/',
-    component: CatalogPage,
-  },
-  {
-    path: '/product/:id/',
-    component: ProductPage,
-  },
-  {
-    path: '/settings/',
-    component: SettingsPage,
-  },
-
-  {
-    path: '/dynamic-route/blog/:blogId/post/:postId/',
-    component: DynamicRoutePage,
-  },
-  {
-    path: '/request-and-load/user/:userId/',
-    async: function (routeTo, routeFrom, resolve, reject) {
-      // Router instance
-      var router = this;
-
-      // App instance
-      var app = router.app;
-
-      // Show Preloader
-      app.preloader.show();
-
-      // User ID from request
-      var userId = routeTo.params.userId;
-
-      // Simulate Ajax Request
-      setTimeout(function () {
-        // We got user data from request
-        var user = {
-          firstName: 'Vladimir',
-          lastName: 'Kharlampidi',
-          about: 'Hello, i am creator of Framework7! Hope you like it!',
-          links: [
-            {
-              title: 'Framework7 Website',
-              url: 'http://framework7.io',
-            },
-            {
-              title: 'Framework7 Forum',
-              url: 'http://forum.framework7.io',
-            },
-          ]
-        };
-        // Hide Preloader
-        app.preloader.hide();
-
-        // Resolve route to load page
-        resolve(
-          {
-            component: RequestAndLoad,
-          },
-          {
-            context: {
-              user: user,
-            }
-          }
-        );
-      }, 1000);
+    {
+        path: '/',
+        component: HomePage,
     },
-  },
-  {
-    path: '(.*)',
-    component: NotFoundPage,
-  },
+
+    // Page with buttons asking if the target is adult or child
+    {
+        path: '/infected/',
+        component: InfectedPage
+    },
+
+    // Page with symptoms quiz
+    {
+        path: '/infected/:age/',
+        component: InfectedQuizPage,
+        on: {
+            pageInit: function(event, page) {
+                console.log(page.route.params.age);
+            },
+        },
+    },
+
+    {
+        path: '/about-covid/',
+        component: AboutCovidPage
+    },
+
+    {
+        path: '/symptoms/',
+        component: SymptomsPage
+    },
+
+    {
+        path: '/risk-group/',
+        component: RiskGroupPage
+    },
+
+    {
+        path: '/prevention/',
+        component: PreventionPage
+    },
+
+    {
+        path: '/where-help/',
+        component: WhereHelpPage
+    },
+
+    {
+        path: '/stats/',
+        component: StatsPage
+    },
+
+    {
+        path: '/news/',
+        component: NewsPage
+    },
+
+    {
+        path: '/mask/',
+        component: MaskPage
+    },
+
+    {
+        path: '/about/',
+        component: AboutPage
+    },
+
+    {
+        path: '/form/',
+        component: FormPage,
+    },
+
+    {
+        path: '/catalog/',
+        component: CatalogPage,
+    },
+
+    {
+        path: '/product/:id/',
+        component: ProductPage,
+    },
+
+    {
+        path: '/settings/',
+        component: SettingsPage,
+    },
+
+    {
+        path: '/dynamic-route/blog/:blogId/post/:postId/',
+        component: DynamicRoutePage,
+    },
+
+    {
+        path: '/request-and-load/user/:userId/',
+        async: function(routeTo, routeFrom, resolve, reject) {
+            // Router instance
+            var router = this;
+
+            // App instance
+            var app = router.app;
+
+            // Show Preloader
+            app.preloader.show();
+
+            // User ID from request
+            var userId = routeTo.params.userId;
+
+            // Simulate Ajax Request
+            setTimeout(function() {
+                // We got user data from request
+                var user = {
+                    firstName: 'Vladimir',
+                    lastName: 'Kharlampidi',
+                    about: 'Hello, i am creator of Framework7! Hope you like it!',
+                    links: [{
+                            title: 'Framework7 Website',
+                            url: 'http://framework7.io',
+                        },
+                        {
+                            title: 'Framework7 Forum',
+                            url: 'http://forum.framework7.io',
+                        },
+                    ]
+                };
+                // Hide Preloader
+                app.preloader.hide();
+
+                // Resolve route to load page
+                resolve({
+                    component: RequestAndLoad,
+                }, {
+                    context: {
+                        user: user,
+                    }
+                });
+            }, 1000);
+        },
+    },
+
+    {
+        path: '(.*)',
+        component: NotFoundPage,
+    },
 ];
 
 export default routes;
