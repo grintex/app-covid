455045a0e3976759e080bc4d0527394707bb2484
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-05-02 13:59:32 -0300

    style: Changes identation from 2 to 4 spaces.

--- src/js/app.js
+++ src/js/app.js
@@ -16,35 +16,35 @@ import routes from './routes.js';
 import App from '../app.f7.html';
 
 var app = new Framework7({
-  root: '#app', // App root element
-  component: App, // App main component
-  id: 'cc.uffs.extension.covid', // App bundle ID
-  name: 'coronavirus-chapeco', // App name
-  theme: 'auto', // Automatic theme detection
+    root: '#app', // App root element
+    component: App, // App main component
+    id: 'cc.uffs.extension.covid', // App bundle ID
+    name: 'coronavirus-chapeco', // App name
+    theme: 'auto', // Automatic theme detection
 
 
 
-  // App routes
-  routes: routes,
+    // App routes
+    routes: routes,
 
 
-  // Input settings
-  input: {
-    scrollIntoViewOnFocus: Framework7.device.cordova && !Framework7.device.electron,
-    scrollIntoViewCentered: Framework7.device.cordova && !Framework7.device.electron,
-  },
-  // Cordova Statusbar settings
-  statusbar: {
-    iosOverlaysWebView: true,
-    androidOverlaysWebView: false,
-  },
-  on: {
-    init: function () {
-      var f7 = this;
-      if (f7.device.cordova) {
-        // Init cordova APIs (see cordova-app.js)
-        cordovaApp.init(f7);
-      }
+    // Input settings
+    input: {
+        scrollIntoViewOnFocus: Framework7.device.cordova && !Framework7.device.electron,
+        scrollIntoViewCentered: Framework7.device.cordova && !Framework7.device.electron,
     },
-  },
+    // Cordova Statusbar settings
+    statusbar: {
+        iosOverlaysWebView: true,
+        androidOverlaysWebView: false,
+    },
+    on: {
+        init: function() {
+            var f7 = this;
+            if (f7.device.cordova) {
+                // Init cordova APIs (see cordova-app.js)
+                cordovaApp.init(f7);
+            }
+        },
+    },
 });
